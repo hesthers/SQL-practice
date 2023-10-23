c428f2cebf959ef8ab547889570e161a6d7147b5
h.esther <89238043+hesthers@users.noreply.github.com>
2023-10-23 22:03:25 +0900

    Time: 539 ms (67.03%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 
-# SELECT S.score, cast(S.rank as unsigned) as 'rank' 
-# FROM (
-  SELECT score
-      , dense_rank() over (order by score desc) AS 'rank'
-  FROM Scores 
-# ) as S
+SELECT score
+    , dense_rank() over (order by score desc) AS 'rank'
+FROM Scores 
+
